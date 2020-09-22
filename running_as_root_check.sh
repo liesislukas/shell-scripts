7a9dd852bac46537375638c2fbf08ad0470e77af
Lukas Liesis <liesislukas@gmail.com>
2020-09-22 10:07:33 +0300

    Update running_as_root_check.sh

--- running_as_root_check.sh
+++ running_as_root_check.sh
@@ -1,6 +1,10 @@
-# running as root or user ?
+# uses another custom function `fn_prompt`
+# text is from Trafikito instalation left as sample, update it to your case
+
+# running as root or user?
 RUNAS=`whoami`
 WHOAMI=`whoami`
+
 if [ "$WHOAMI" != "root" ]; then
     echo "If possible, run installation as root user."
     echo "To install as root either log in as root and execute the script or use:"
