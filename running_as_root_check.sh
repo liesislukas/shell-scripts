714e177b6ace80dd94665ebb14d2a776cb0e8739
Lukas Liesis <liesislukas@gmail.com>
2020-09-22 10:06:33 +0300

    Create running_as_root_check.sh

---
+++ running_as_root_check.sh
@@ -0,0 +1,12 @@
+# running as root or user ?
+RUNAS=`whoami`
+WHOAMI=`whoami`
+if [ "$WHOAMI" != "root" ]; then
+    echo "If possible, run installation as root user."
+    echo "To install as root either log in as root and execute the script or use:"
+    echo
+    echo "  sudo sh $0 --user_api_key=$USER_API_KEY --workspace_id=$WORKSPACE_ID --servername=$SERVER_NAME"
+    echo
+    fn_prompt "N" "Continue as $WHOAMI [yN]: " || exit 1
+    RUNAS="$WHOAMI"
+fi
