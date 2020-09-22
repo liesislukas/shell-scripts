56ae0a1a6b3634daea46715ceb2bd25fe92d5146
Lukas Liesis <liesislukas@gmail.com>
2020-09-22 09:54:33 +0300

    Update log.sh

--- log.sh
+++ log.sh
@@ -5,8 +5,12 @@ export BASEDIR=$1
 # create prev logs file and clear current logs file
 export LOGFILE=$BASEDIR/logs.log
 if [ -f $LOGFILE ]; then
+  # This would always clean up log file and put older log to logs.log.prev file
   cp $LOGFILE $LOGFILE.prev
   echo "" >$LOGFILE
+  # if want to better keep last 1000 lines of log to bak file, use this:
+  # cp $LOGFILE $LOGFILE.bak
+  # tail -n 1000 $LOGFILE.bak >$LOGFILE
 fi
 
 ###################################################
